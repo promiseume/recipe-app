2acc1b74da3c978ba6f0376ca9ce7092d31a8a29
Promise Ume <ngoziume1@gmail.com>
2021-12-17 12:15:26 +0100

    styling

--- src/component/landingPage/header.js
+++ src/component/landingPage/header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <div className='header'>
             <div className='header-text'>
           <h3>Let's Start <br/>Cooking With <br/> Popular Recipes</h3>
-          <p>want to make a dish but don't know where to start?<br/> No need to worry again!</p>
+          <p>Want to make a dish but don't know where to start?<br/> No need to worry again!</p>
             </div>
             <div className='header-img'>
                 <img src={img} alt='header image'/>
